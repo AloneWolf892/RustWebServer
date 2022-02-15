4220455d5e5faa64f0398f5f9bbd9fa5592404df
Cesar Enrique Bello Martiña <60964787+AloneWolf892@users.noreply.github.com>
2022-02-15 15:56:23 +0100

    web server functional

---
+++ src/bin/main.rs
@@ -0,0 +1,95 @@
+use std::fs;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+use std::io::Read;
+use std::io;
+use webserver::ThreadPool;
+use webserver::directory_list;
+
+fn main() {
+
+    let mut hostname = String::new();
+    let mut root = String::new();
+
+    println!("Insert the hostname of the server and the port [Example = \"127.0.0.1:80]\"");
+    io::stdin().read_line(&mut hostname).expect("Failed to read line");
+    let hostname = hostname.trim();
+
+    println!("Insert the root of the server");
+    io::stdin().read_line(&mut root).expect("Failed to read the line");
+    let root = root.trim();
+
+    let listener = TcpListener::bind(hostname).unwrap();
+    let pool = ThreadPool::new(4);
+    
+    for stream in listener.incoming() {
+        let file_structure = directory_list(String::from(root));
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream, file_structure);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream, file_structure: Vec<String>) {
+
+    let mut file_structure = file_structure;
+
+    let mut buffer = [0; 1024];
+
+    let mut status_line = "HTTP/1.1 200 OK";
+    let mut filename = "websiteroot/index.html";
+
+    stream.read(&mut buffer).unwrap();
+
+    for entry in (file_structure.iter_mut()).rev() {
+
+        if entry.contains("404.html") {
+            continue;
+        }
+        let mut site = entry.replace("websiteroot/", "").replace("index.html", "");
+        site = format!("GET /{site} HTTP/1.1\r\n");
+        let site = site.as_bytes();
+
+        if buffer.starts_with(site) {
+            status_line = "HTTP/1.1 200 OK";
+            filename = entry;
+            break;
+        } else {
+            status_line = "HTTP/1.1 404 NOT FOUND";
+            filename = "websiteroot/404.html";
+        }
+    }
+
+    if filename.contains(".png") || filename.contains(".jpg") || filename.contains(".gif") || filename.contains(".jpeg") {
+        let contents = fs::read(filename);
+        let contents = match contents {
+            Ok(contents ) => contents,
+            Err(error) => panic!("Problem opening file: {:?}", error)
+        };
+
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n",
+            status_line, &contents[..].len()
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.write(&contents[..]).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let contents = fs::read_to_string(filename);
+        let contents = match contents {
+            Ok(contents) => contents,
+            Err(error) => panic!("Problem opening file: {:?}", error)
+        };
+
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line, contents.len(), contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,105 @@
+use std::{thread, sync::{mpsc, Arc, Mutex}};
+use walkdir::WalkDir;
+
+pub fn directory_list(root: String) -> Vec<String> {
+    let mut file_structure: Vec<String> = Vec::new(); 
+    let mut reduced_file_structure: Vec<String> = Vec::new();
+    for entry in WalkDir::new(&root) {
+        match entry {
+            Ok(path) => file_structure.push(String::from(path.path().to_str().unwrap())),
+            Err(err) => println!("{}", err)
+        }
+    }
+
+    for entry in file_structure.iter_mut() {
+        if entry.contains(".") {
+            *entry = entry.replace("\\", "/");
+            reduced_file_structure.push(String::from(entry.clone()));
+        }
+    }
+
+    reduced_file_structure
+
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where 
+        F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap()
+        }
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker: {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop{
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();
+                },
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
+        });
+
+        Worker { id, thread: Some(thread) }
+    }
+}
